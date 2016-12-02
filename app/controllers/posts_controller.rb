8e04eda63a603a46e4f37ff05e466a8b898d915c
Catherine Kwak <khopsickle@users.noreply.github.com>
2016-12-02 11:11:35 -0700

    Finish edit page, show page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,11 @@
 class PostsController < ApplicationController
-  before_action :find_post, only: [:edit, :show]
+  before_action :find_post, only: [:edit, :show, :update]
 
   def edit
   end
 
   def index
-    @posts = Post.all
+    @posts = Post.all.order("created_at DESC")
   end
 
   def show
@@ -15,8 +15,20 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def update
+    if @post.update(whitelist)
+      redirect_to @post
+    else
+      render :edit
+    end
+  end
+
   private
     def find_post
       @post = Post.find_by_id(params[:id])
+    end
+
+    def whitelist
+      params.require(:post).permit(:title, :body, :category_id, tag_ids: [])
     end
 end
